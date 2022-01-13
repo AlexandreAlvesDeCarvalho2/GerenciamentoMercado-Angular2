e60dd9c3942abfa1f22dcd9ad5e36f985a60f4cd
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2022-01-13 18:50:43 -0300

    adicionando componente.ts e service.ts

--- src/app/login-page/login/login.component.ts
+++ src/app/login-page/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../model/login';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService){}
 
   ngOnInit(): void {
   }
 
+
+
+PostMethod() {
+  const NewLogin: Login = {
+
+    idusuario: 0,
+    email: (<HTMLInputElement>document.getElementById('email')).value,
+    senha: (<HTMLInputElement>document.getElementById('senha')).value,
+    cpf: ''
+
+  };
+
+  console.log(NewLogin);
+  this.loginService.PostService(NewLogin).subscribe(
+    (sucesso) => console.log('sucesso'),
+    (error) => console.log(error),
+    () => console.log
+    );
+    {alert("test")}
+}
 
 
 
--- src/app/login-page/services/login.service.ts
+++ src/app/login-page/services/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,15 @@ export class LoginService {
 
   constructor(private httpClient: HttpClient) {}
   private readonly API = 'api';
+
+
+  PostService(NewRegistro: any){
+
+    window.location.reload()
+    console.log(this.API+"/Logar")
+    return this.httpClient.post(this.API + "/Logar", NewRegistro).pipe(take(1));
+
+  }
+
+
 }
